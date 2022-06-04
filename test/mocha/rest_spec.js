6f9714236995c4040047a0b694ae016c76bb22cb
David J Birnbaum <djbpitt@gmail.com>
2022-06-04 17:36:27 -0400

    Fix app name in mocha/rest_spec.js to fix CI

--- test/mocha/rest_spec.js
+++ test/mocha/rest_spec.js
@@ -31,7 +31,7 @@ describe('rest api returns', function () {
 
     it.skip('file index.html exists in application root', function (done) {
       client
-        .get('/exist/rest/db/apps/tmp/index.html')
+        .get('/exist/rest/db/apps/pr-app/index.html')
         .expect(200)
         .end(function (err, res) {
           expect(res.status).to.equal(200)
