3cfb3e76ef028a9b39678a78c1572e5e822d4425
David J Birnbaum <djbpitt@gmail.com>
2022-07-07 17:50:31 -0400

    Remove test-tmp to try to repair CI

--- test-tmp/cypress/integration/landing_spec.js
+++
@@ -1,9 +0,0 @@
-/* global cy */
-describe('The landing page', function () {
-  it.skip ('should load ', function () {
-    cy.visit('/exist/apps/pr-app/index.html')
-      .get('.alert')
-      .contains('app.xql')
-  })
-
-})
